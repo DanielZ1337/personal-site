a1d46e4e25b9c17bf25633ed9b386abf56490f5f
DanielZ1337 <danielz2nd@hotmail.com>
2023-08-18 19:45:18 +0200

    fix contactform layout

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -16,10 +16,10 @@ export default function ContactForm({dict}: { dict: Dictionary }) {
         >
             <div
                 id={dict.navbar.links[0].contact.id}
-                className={"mt-16 mb-16 flex md:max-w-[70vw] max-w-full w-full mx-auto bg-neutral-200 dark:bg-black rounded-xl py-24 px-20 flex-col h-full shadow-md"}>
+                className={"mt-16 mb-16 flex max-w-[90vw] lg:max-w-[70vw] mx-auto bg-neutral-200 dark:bg-black rounded-xl py-24 px-20 flex-col h-full shadow-md"}>
                 <h1 className={"text-5xl font-bold text-center"}>{dict.text.contacttitle}</h1>
                 <h2 className={"text-2xl mt-6 mb-8 dark:text-muted-foreground text-center"}>{dict.text.contact}</h2>
-                <div className={"mx-auto flex gap-2 w-1/2"}>
+                <div className={"mx-auto flex gap-2 w-full md:w-2/3 xl:w-1/2"}>
                     <div className={"whitespace-pre-wrap flex-wrap w-full"}>
                         <p className={"text-xl mb-5 md:mt-0 mt-16 dark:text-muted-foreground text-start"}>{dict.text.email}: <span>
                 <Link href={`mailto:${siteConfig.links.email}`}
